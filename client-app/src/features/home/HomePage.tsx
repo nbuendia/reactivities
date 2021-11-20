e5101dd5747cfc2e2d64244864403b7dd4912b51
nbuendia <nicole.buendia424@gmail.com>
2021-11-20 18:38:10 -0500

    added new home component (landing page)

---
+++ client-app/src/features/home/HomePage.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+//SEMANTIC UI
+import { Container } from "semantic-ui-react";
+
+function HomePage() {
+    return(
+        <Container style={{marginTop: '7em'}}>
+            <h1>Home Page</h1>
+            <h3>Go To <Link to='/activities'>Activities</Link></h3>
+        </Container>
+    )
+}
+
+export default HomePage;
