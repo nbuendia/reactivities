00158426330e19e72c9df453379bd3fcc1641e4b
nbuendia <nicole.buendia424@gmail.com>
2021-11-20 18:21:14 -0500

    added link paths to nav bar

--- client-app/src/app/layout/NavBar.tsx
+++ client-app/src/app/layout/NavBar.tsx
@@ -1,22 +1,20 @@
 import React from "react";
-import { useStore } from "../stores/store";
 
 //SEMANTIC UI
 import { Button, Container, Menu } from "semantic-ui-react";
+import { NavLink } from "react-router-dom";
 
 export default function NavBar() {
-    const { activityStore } = useStore();
-
     return(
         <Menu inverted fixed='top'>
             <Container>
-                <Menu.Item header>
+                <Menu.Item as={NavLink} to='/' exact header>
                     <img src='/assets/logo.png' alt='logo' style={{marginRight: '10px'}}/>
                     Reactivities
                 </Menu.Item>
-                <Menu.Item name='Activities' />
+                <Menu.Item as={NavLink} to='/activities' name='Activities' />
                 <Menu.Item>
-                    <Button positive content='Create Activity' onClick={() => activityStore.openForm()}/>
+                    <Button as={NavLink} to='/createActivity' positive content='Create Activity' />
                 </Menu.Item>
             </Container>
         </Menu>
