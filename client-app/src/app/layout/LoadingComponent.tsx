c5d959ce338c3dddb5bff664bdce28f470f45620
nbuendia <nicole.buendia424@gmail.com>
2021-11-20 18:20:01 -0500

    fixed typo

--- client-app/src/app/layout/LoadingComponent.tsx
+++ client-app/src/app/layout/LoadingComponent.tsx
@@ -11,7 +11,7 @@ interface Props {
 export default function LoadingComponent({inverted = true, content = 'Loading...'}: Props) {
     return(
         <Dimmer active={true} inverted={inverted} >
-            <Loader contect={content}/>
+            <Loader content={content}/>
         </Dimmer>
     )
 }
