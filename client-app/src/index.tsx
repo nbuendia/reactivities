3e964e60869ee461c47815a99024b60349cade33
nbuendia <nicole.buendia424@gmail.com>
2021-11-20 12:22:02 -0500

    wrapped APP component to contain MOBX store so all components can have access to store and props wont need to be passed down

--- client-app/src/index.tsx
+++ client-app/src/index.tsx
@@ -1,14 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { store, StoreContext } from './app/stores/store';
+
 import './app/layout/styles.css';
 import 'semantic-ui-css/semantic.min.css';
+
+//COMPONENT
 import App from './app/layout/App';
+
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  // <React.StrictMode>
-    <App />,
-  // </React.StrictMode>,
+  <StoreContext.Provider value={store}>
+    <App />
+  </StoreContext.Provider>,
   document.getElementById('root')
 );
 
