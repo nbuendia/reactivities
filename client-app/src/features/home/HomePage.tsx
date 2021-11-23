3b377faa792af1a6123af4778518a1f5c7324033
nbuendia <nicole.buendia424@gmail.com>
2021-11-23 13:11:35 -0500

    finished up landingh page-added style and button to take you to activities page

--- client-app/src/features/home/HomePage.tsx
+++ client-app/src/features/home/HomePage.tsx
@@ -2,14 +2,22 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 //SEMANTIC UI
-import { Container } from "semantic-ui-react";
+import { Button, Container, Header, Image, Segment } from "semantic-ui-react";
 
 function HomePage() {
     return(
-        <Container style={{marginTop: '7em'}}>
-            <h1>Home Page</h1>
-            <h3>Go To <Link to='/activities'>Activities</Link></h3>
-        </Container>
+        <Segment inverted textAlign='center' vertical className='masthead' >
+            <Container text>
+                <Header as='h1' inverted>
+                    <Image size='massive' src='/assets/logo.png' alt='logo' style={{marginBootom: 12}} />
+                    Reactivities
+                </Header>
+                <Header as ='h2' inverted content='Welcome to Reactivites' />
+                <Button as={Link} to='/activities' size='huge' inverted>
+                    Take Me To The Activities
+                </Button>
+            </Container>
+        </Segment>
     )
 }
 
