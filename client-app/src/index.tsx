c89f62c9707216e1645715951ecb434567734035
nbuendia <nicole.buendia424@gmail.com>
2021-11-25 09:44:32 -0500

    changed browserRouter to router, imprted history to use for error handling and added toast styling

--- client-app/src/index.tsx
+++ client-app/src/index.tsx
@@ -1,22 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {createBrowserHistory} from 'history';
 import { store, StoreContext } from './app/stores/store';
 
-import 'react-calendar/dist/Calendar.css';
-import './app/layout/styles.css';
+//STYLES
 import 'semantic-ui-css/semantic.min.css';
+import 'react-calendar/dist/Calendar.css';
+import 'react-toastify/dist/ReactToastify.min.css';
+import './app/layout/styles.css';
 
 //COMPONENT
 import App from './app/layout/App';
 
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter } from 'react-router-dom';
+import { Router } from 'react-router-dom';
+
+export const history = createBrowserHistory();
 
 ReactDOM.render(
   <StoreContext.Provider value={store}>
-    <BrowserRouter>
+    <Router history={history}>
       <App />
-    </BrowserRouter>
+    </Router>
   </StoreContext.Provider>,
   document.getElementById('root')
 );
