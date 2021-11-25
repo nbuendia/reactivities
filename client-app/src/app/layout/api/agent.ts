4cb5a2c2162a78f710481af635a18f31af258ec6
nbuendia <nicole.buendia424@gmail.com>
2021-11-25 09:42:41 -0500

    added toast, history and store to handle errors with switch statement

--- client-app/src/app/layout/api/agent.ts
+++ client-app/src/app/layout/api/agent.ts
@@ -1,4 +1,7 @@
-import axios, { AxiosResponse } from "axios";
+import axios, { AxiosError, AxiosResponse } from "axios";
+import { toast } from "react-toastify";
+import { history } from "../../..";
+import { store } from "../../stores/store";
 import { Activity } from "../models/activity";
 
 const sleep = (delay: number) => {
@@ -10,13 +13,42 @@ const sleep = (delay: number) => {
 axios.defaults.baseURL = 'http://localhost:5000/api';
 
 axios.interceptors.response.use(async response => {
-    try {
         await sleep(1000);
         return response;
-    } catch (error) {
-        console.log(error);
-        return await Promise.reject(error);
+}, (error: AxiosError) => {
+    const {data, status, config} = error.response!;
+    switch (status) {
+        case 400:
+            if (typeof data == 'string') {
+                toast.error(data);
+            }
+            if (config.method == 'get' && data.errors.hasOwnProperty('id')) {
+                history.push('/not-found');
+            }
+            if (data.errors) {
+                const modelStateErrors = [];
+                for (const key in data.errors) {
+                    if (data.errors[key]) {
+                        modelStateErrors.push(data.errors[key])
+                    }
+                }
+                throw modelStateErrors.flat();
+            }
+            break;
+        case 401:
+            toast.error('Unauthorized');
+            break;
+        case 404:
+            history.push('/not-found');
+            break;
+        case 500:
+            store.commonStore.setServerError(data);
+            history.push('/server-error');
+            break;
+        default:
+            break;
     }
+    return Promise.reject(error);
 })
 
 const responseBody = <T> (response: AxiosResponse<T>) => response.data;
