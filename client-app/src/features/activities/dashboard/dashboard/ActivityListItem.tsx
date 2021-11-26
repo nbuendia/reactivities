cdb9503fb91633d440375015193d427c30d06fe5
nbuendia <nicole.buendia424@gmail.com>
2021-11-26 10:58:41 -0500

    formatted date with date-fns

--- client-app/src/features/activities/dashboard/dashboard/ActivityListItem.tsx
+++ client-app/src/features/activities/dashboard/dashboard/ActivityListItem.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { Button, Icon, Item, Segment } from "semantic-ui-react";
 import { Activity } from "../../../../app/layout/models/activity";
+
+//SEMANTIC UI
+import { Button, Icon, Item, Segment } from "semantic-ui-react";
+
+import {format} from 'date-fns';
 
 interface Props {
     activity: Activity
@@ -25,7 +29,7 @@ function ActivityListItem ({activity}: Props) {
             </Segment>
             <Segment>
                 <span>
-                    <Icon name='clock' /> {activity.date}
+                    <Icon name='clock' /> {format(activity.date!, 'MMM dd yyyy h:mm aa')}
                     <Icon name='marker' /> {activity.venue}
                 </span>
             </Segment>
